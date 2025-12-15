5232bb7b291670242c5a5278f1d74074bba30cf0
Eduardo Goytendia <egoytendia@dialdata.com.ar>
2025-12-15 19:15:42 -0300

    ejercios del 1 al 9, falta definir mcd y queda todo listo

---
+++ Guia-01/Ejercicio-1-a-9.hs
@@ -0,0 +1,29 @@
+esMultiploDeTres :: Int -> Bool
+esMultiploDeTres x = x `mod` 3 == 0
+
+esMultiploDe :: Int -> Int -> Bool
+esMultiploDe x y = y `mod` x == 0 
+
+cubo :: Int -> Int
+cubo x = x * x * x
+
+area :: Int -> Int -> Int
+area x y = x * y
+
+esBisiesto ::  Int -> Bool
+esBisiesto x =  esMultiploDe 400 x || (esMultiploDe 4 x && not(esMultiploDe 100 x))
+
+celsiusToFahr :: Int -> Float
+celsiusToFahr x = (fromIntegral x * 9 / 5) + 32
+
+fahrToCelsius :: Float -> Float
+fahrToCelsius x = (x - 32) * 5 / 9
+
+haceFrioF :: Float -> Bool
+haceFrioF x = x < celsiusToFahr 8 
+
+mcm :: Int -> Int -> Int
+mcm x y =  (x * y) `div` mcd x y 
+
+mcd :: Int -> Int -> Int
+mcd x y = 
